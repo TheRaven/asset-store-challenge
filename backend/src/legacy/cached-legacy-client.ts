fee7031309ad6fb02c798687e3c3f2a399538b9b
Yannick Simard <yannick.simard@unity3d.com>
2025-01-22 16:50:30 -0500

    Refactor code to use a single method for both operations

--- backend/src/legacy/cached-legacy-client.ts
+++ backend/src/legacy/cached-legacy-client.ts
@@ -1,18 +1,36 @@
+
 import { Client } from "./client";
 import { Product } from "./products";
-import axios from 'axios';
-
+import { RedisClientType } from "redis";
 
 export class CachedLegacyClient implements Client {
 
-    constructor(private readonly client: Client) {}
-
+    constructor(private readonly redisClient: RedisClientType, private readonly cacheDurationInSeconds: number, private readonly client: Client) {}
 
     public async getProducts(): Promise<Product[]> {
-      return this.client.getProducts();
+      return this.fetchAndCache(`products`, () => this.client.getProducts());
     }
 
-    public async getPrice(productId: string): Promise<Product> {
-      return this.client.getPrice(productId);
+    public async getPrice(productId: string): Promise<number> {
+      return this.fetchAndCache(`price:${productId}`, () => this.client.getPrice(productId));
     }
+
+
+    private async fetchAndCache<T>(cacheKey: string, fetch: () => Promise<T>): Promise<T> {
+      const cachedData = await this.redisClient.get(cacheKey);
+
+      if(cachedData) {
+        console.debug(`Found ${cacheKey} in cache`);
+        return JSON.parse(cachedData);
+      }
+
+      console.debug(`Fetching fetching data from remote service`);
+      const data = await fetch();
+
+      console.debug(`Caching ${cacheKey}`);
+      await this.redisClient.set(cacheKey, JSON.stringify(data), {EX: this.cacheDurationInSeconds});
+
+      return data;
+    };
+
 }
